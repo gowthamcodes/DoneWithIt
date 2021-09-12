6d1bccdad3217bfaf2bf624c997e4d5dac35e732
gowthamcodes <gowthamcodes@gmail.com>
2021-09-12 23:38:44 +0530

    stack navigation - basic implementation

--- App.js
+++ App.js
@@ -1,27 +1,36 @@
 import React from "react";
 import { Button, Text } from "react-native";
-
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+
 import Screen from "./app/components/Screen";
 
-const tweets = ({ navigation: { navigate } }) => (
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button title="Link" onPress={() => navigation.navigate("TweetDetails")} />
+  );
+};
+
+const Tweet = ({ navigation }) => (
   <Screen>
-    <Text>Tweets</Text>
-    <Button title="Go to" onPress={() => navigate("TweetsDetails")} />
+    <Text>Tweet Screen</Text>
+    {/* <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} /> */}
+    <Link />
   </Screen>
 );
-const tweetDetails = () => (
+
+const TweetDetails = () => (
   <Screen>
-    <Text>Tweet Details</Text>
+    <Text>Tweet Details Screen</Text>
   </Screen>
 );
 
 const Stack = createStackNavigator();
 const StackNavigator = () => (
   <Stack.Navigator>
-    <Stack.Screen name="Tweets" component={tweets} />
-    <Stack.Screen name="TweetsDetails" component={tweetDetails} />
+    <Stack.Screen name="Tweet" component={Tweet} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
   </Stack.Navigator>
 );
 
