45c91b7a0225236d0251c4581c017cbf4c65ad41
gowthamcodes <gowthamcodes@gmail.com>
2021-11-01 01:05:19 +0530

    release build

--- app/api/client.js
+++ app/api/client.js
@@ -1,9 +1,10 @@
 import { create } from "apisauce";
 import authStorage from "../auth/storage";
 import cache from "../utils/cache";
+import settings from "../utils/settings";
 
 const apiClient = create({
-  baseURL: "http://192.168.13.25:9000/api",
+  baseURL: settings.apiUrl,
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
